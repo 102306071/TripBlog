cc69ee4bd244479a68d410be292b2144b905d40a
102306071 <102306071@nccu.edu.tw>
2018-06-17 03:29:43 +0800

    add about.php

---
+++ about.php
@@ -0,0 +1,53 @@
+<html lang="en">
+  <body>
+	<?php include "header.php"; ?>
+    <!-- Navigation -->
+    <?php include "navigation.php"; ?>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/eva-air.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>關於我</h1>
+              <!-- <span class="subheading">用旅行感受生命 以相機記錄人生</span> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <p class="text-center">
+
+            單純的 <br>
+            喜歡旅行 <br><br>  
+
+            單純的 <br>
+            喜歡拍照 <br><br>
+
+            單純的<br>
+            想用最單純的方式<br><br>
+
+            紀錄僅此一次的<br>  
+            年輕<br>
+          </p> <br>
+
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "footer.php"; ?>
+
+  </body>
+
+</html>
+
