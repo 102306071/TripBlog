8c1ecae6b543c26254fa560b55dc45924b68b415
102306071 <102306071@nccu.edu.tw>
2018-06-17 02:18:16 +0800

    first commit

---
+++ FY18_America.php
@@ -0,0 +1,307 @@
+<html lang="en">
+  <body>
+    <?php include "header.php"; ?>
+    <!-- Navigation -->
+    <?php include "navigation.php"; ?>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/pos-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1>2018 美東16日</h1>
+              <h2 class="subheading">Vancouver, New York, Niagara, Boston</h2>
+              <span class="meta">Posted by
+                <a href="#">Ernie Y. Wu</a>
+                on July 11, 2018</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <!-- 1 航班資訊 -->
+            <div>
+              <a data-toggle="collapse" href="#flight" aria-expanded="false" aria-controls="flight">
+                <h2>航班資訊
+                  <a href="file/電子機票.pdf" target="_blank">
+                    <i class="fa fa-paperclip fa-1x"></i><br>
+                  </a>
+                </h2>
+              </a>
+
+              <div class="collapse" id="flight">
+                <div class="card card-body">
+                  <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">Airport</th>
+                        <th scope="col">Time</th>
+                        <th scope="col">Flight</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">2018/7/11 <br>(Wed)</th>
+                        <td>台北桃園 (TPE) Terminal 2 ～ <br>
+                          溫哥華 (YVR) Terminal M</td>
+                        <td>15:55 ～ <br>
+                          11:45 (10hr50)</td>
+                        <td>AC018-Air Canada Economy(A)</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/12 <br>(Thu)</th>
+                        <td>溫哥華 (YVR) Terminal M ～ <br>
+                          紐瓦克 (EWR) Terminal A</td>
+                        <td>8:45 ～ <br>
+                          16:50 (5hr05)</td>
+                        <td>AC548-Air Canada Economy(A)</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/26 <br>(Thu)</th>
+                        <td>波士頓 (BOS) Terminal B ～ <br>
+                          溫哥華 (YVR) Terminal M</td>
+                        <td>18:05 ～ <br>
+                          21:17 (6hr12)</td>
+                        <td>AC533-Air Canada Economy(A)</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/27 <br>(Fri)</th>
+                        <td>溫哥華 (YVR) Terminal M ～ <br>
+                          台北桃園 (TPE) Terminal 2 </td>
+                        <td>11:10 ～ <br>
+                          14:15 (12hr05) +1day</td>
+                        <td>AC017-Air Canada Economy(A)</td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+              </div>  
+            </div>
+            
+            <!-- 2 住宿資訊-->
+            <div>
+              <a data-toggle="collapse" href="#hotel" aria-expanded="false" aria-controls="hotel">
+                <h2>住宿資訊</h2> 
+              </a>
+              <div class="collapse" id="hotel">
+                <div class="card card-body">
+                  <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">Hotel</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Notes</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">2018/7/11 ~ 2018/7/12</th>
+                        <td>Ramada Limited Vancouver Airport</td>
+                        <td>
+                          <a href="https://goo.gl/maps/DTbSj8TLM5J2" target="_blank">
+                          7188 Westminster Hwy, Richmond, BC, V6X1A1 Canada
+                          </a>
+                        </td>
+                        <td>1 room |  1 night
+                          <a href="file/住宿_0711.pdf" target="_blank">
+                            <i class="fa fa-paperclip fa-1x"></i><br>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/12 ~ 2018/7/21</th>
+                        <td>The Local, Long Island City</td>
+                        <td>
+                          <a href="https://goo.gl/maps/8L8kFbypxux" target="_blank">
+                          13-02 44th Avenue, Long Island City, NY, 11101 United States of America
+                          </a>
+                        </td>
+                        <td> 1 room |  9 nights
+                          <a href="file/住宿_0712.pdf" target="_blank">
+                            <i class="fa fa-paperclip fa-1x"></i><br>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/14 ~ 2018/7/15</th>
+                        <td>Hostel Buffalo-Niagara</td>
+                        <td>
+                          <a href="https://goo.gl/maps/fw1F17dgCWD2" target="_blank">
+                          667 Main St., Buffalo, NY, 14203 United States of America
+                          </a>
+                        </td>
+                        <td>1 room |  1 night</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2018/7/21 ~ 2018/7/26</th>
+                        <td>HI Boston, Boston </td>
+                        <td>
+                          <a href="https://goo.gl/maps/FrFjRDV5ZeQ2" target="_blank">
+                          19 Stuart St, Boston, MA, 02116 United States of America
+                          </a>
+                        </td>
+                        <td>1 room |  5 night <br> 
+                          現場支付 US$292.49
+                          <a href="file/住宿_0711.pdf" target="_blank">
+                            <i class="fa fa-paperclip fa-1x"></i><br>
+                          </a> </td>
+                      </tr>
+
+                    </tbody>
+                  </table>                
+                </div>
+              </div>               
+            </div>
+
+            <!-- 3 Day1 -->
+            <div>
+              <a data-toggle="collapse" href="#day1" aria-expanded="false" aria-controls="day1">
+                <h2>Day1
+                </h2>
+              </a>
+
+              <div class="collapse" id="day1">
+                <div class="card card-body">
+                  <p>this is a sample block</p>                  
+                </div>
+              </div>  
+            </div>
+
+            <!-- 4 Day2 -->
+            <div>
+              <a data-toggle="collapse" href="#day2" aria-expanded="false" aria-controls="day2">
+                <h2>Day2
+                </h2>
+              </a>
+
+              <div class="collapse" id="day2">
+                <div class="card card-body">
+                  <h3> 2018/7/12 (Thu)</h3>
+                  <li>16:50 抵達紐瓦克機場</li>
+<!--                   <li>離紐約最近的Outlet - Jersey Garden</li>
+                    <div class="embed-responsive embed-responsive-16by9">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24208.62465878033!2d-74.17698177922385!3d40.67224848663496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24d80341e148b%3A0x342ea3cb57d4d522!2z5r6k6KW_6Iqx5ZyS6LO854mp5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1528526313784" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div> -->
+                  <li>搭乘Air Train 轉接到紐約地鐵/公車(NY Penn Station)</li>
+                    <img src="img/newark-airport-manhattan-airtrain-map.jpg" class="img-fluid" alt="Responsive image">
+                    <br>
+                    <div class="embed-responsive embed-responsive-16by9" >
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81361.22335529544!2d-74.15943971763012!3d40.72672107102707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb3f3b437c5d7f286!2z57SQ55Om5YWL6Ieq55Sx5ZyL6Zqb5qmf5aC0!5e0!3m2!1szh-TW!2stw!4v1528525607774" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                 
+                    </div>
+                  NY Penn Station to The Local Hostel
+                    <div class="embed-responsive embed-responsive-16by9">                    
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d24178.24771532334!2d-73.98727664999376!3d40.75584472691935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x89c259ae15b2adcb%3A0x7955420634fd7eba!2zUGVubiBTdGF0aW9uLCDntJDntITntJDntITlt57nvo7lnIs!3m2!1d40.750568!2d-73.99351899999999!4m5!1s0x89c25927fcd001a9%3A0x58cecc0f3f00b577!2zVGhlIExvY2FsIE5ZQywgNDR0aCBBdmVudWUsIOmVt-WztuW4gue0kOe0hOW3nue-juWciw!3m2!1d40.749595!2d-73.947825!5e0!3m2!1szh-TW!2stw!4v1528525911745" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <br>
+                  <li>龍門廣場州立公園Gantry Plaza State Park</li>
+                  <p>公園最醒目的標記是兩對鋼鐵造的龍門起吊架，上面醒目的大字「Long Island」，記錄著100多年前的輝煌。</p>
+                    <div class="embed-responsive embed-responsive-16by9 ">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.7508437232996!2d-73.96093718473793!3d40.74550767932835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a220d00e0ed%3A0x69ea605d6e14b839!2sGantry+Plaza+State+Park!5e0!3m2!1szh-TW!2stw!4v1528528173231" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>  
+                
+                </div>
+              </div>  
+            </div>
+
+            <!-- 5 Day3 -->
+            <div>
+              <a data-toggle="collapse" href="#day3" aria-expanded="false" aria-controls="day3">
+                <h2>Day3
+                </h2>
+              </a>
+
+              <div class="collapse" id="day3">
+                <div class="card card-body">
+                  <h3> 2018/7/13 (Fri)</h3>  
+                  [上午]  
+                  <li>自由女神 Statue of Liberty National Monument</li>   
+                  地鐵4、5號線到Bowling Green站，從Battery Park搭乘渡輪（來回$18）
+                    <div class="embed-responsive embed-responsive-16by9"> 
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d608.9100599659885!2d-74.0167650539264!3d40.702197292430476!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a126480c6e5%3A0x6a6ebc63ffaa4d8f!2sBattery+Park!5e0!3m2!1szh-TW!2stw!4v1529137186342" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                   
+                    </div>
+
+                    <br>
+                  [下午] 下城區
+                  <li>華爾街 Wall Street</li>
+                  <li>銅牛 Charging Bull</li>
+                  <li>紐約證券交易所 New York Stock Exchange</li>
+                  <li>聯邦國家紀念堂 Federal Hall National Memorial</li>
+                    <div class="embed-responsive embed-responsive-16by9">                    
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4947.536707742638!2d-74.01524675067111!3d40.70641108760176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2589a17a81551%3A0xc7db393ae1eff521!2z6I-v54i-6KGX6YqF54mb!5e0!3m2!1szh-TW!2stw!4v1529132910531" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> 
+                    </div>
+                    <br>
+                  [晚上] 布魯克林大橋夜景
+                    <div class="embed-responsive embed-responsive-16by9">                    
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10866.37202068108!2d-74.00112599661998!3d40.70231377109067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a2343ce7b2b%3A0x2526ddba7abd465c!2z5biD6a2v5YWL5p6X5aSn5qmL!5e0!3m2!1szh-TW!2stw!4v1529169805194" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>                
+                </div>
+              </div>  
+            </div>
+
+            <!-- 6 Day4 -->
+            <div>
+              <a data-toggle="collapse" href="#day4" aria-expanded="false" aria-controls="day4">
+                <h2>Day4
+                </h2>
+              </a>
+
+              <div class="collapse" id="day4">
+                <div class="card card-body">
+                  <h3>2018/7/14 (Sat)</h3>
+                  <li>11:15 搭乘Megabus，前往Buffalo
+                      <a href="file/MEGABUS_bookingconfirmation.pdf" target="_blank">
+                        <i class="fa fa-paperclip fa-1x"></i><br>
+                      </a>                    
+                  </li>
+                  New York, NY, 34th St b/t 11th Ave and 12th Ave 
+                    <div class="embed-responsive embed-responsive-16by9">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.4749509006226!2d-74.00334791107278!3d40.75608865694847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259b381df75b9%3A0x90a02972be4393a8!2s34th+St+b%2Ft+11th+Ave+and+12th+Ave!5e0!3m2!1szh-TW!2stw!4v1529171686188" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                    
+                    </div>
+                  <li>20:55抵達 Buffalo，前往Hostel Buffalo-Niagara Check-in </li>
+                    <div class="embed-responsive embed-responsive-16by9">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d5846.604195718378!2d-78.87710687360698!3d42.88757257755158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x89d31245591cbf27%3A0x4b258d7041f39771!2zSG9zdGVsIEJ1ZmZhbG8tTmlhZ2FyYSA35pyIMTTml6UgLSA35pyIMTXml6U!3m2!1d42.892078999999995!2d-78.871726!4m5!1s0x89d31237f958425d%3A0x67fc1c159a319c5c!2zRG93bnRvd24gVGVybWluYWwsIEJ1ZmZhbG8sIE5ZLCBCdWZmYWxvLCBOWSAxNDIwM-e-juWciw!3m2!1d42.883038!2d-78.872582!5e0!3m2!1szh-TW!2stw!4v1529172245138" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                                   
+                    </div>
+                </div>
+              </div>  
+            </div>
+
+
+            <!-- sample -->
+            <div>
+              <a data-toggle="collapse" href="#example" aria-expanded="false" aria-controls="example">
+                <h2>自由行懶人包
+                </h2>
+              </a>
+
+              <div class="collapse" id="example">
+                <div class="card card-body">
+                  <a href="http://anrine910070.pixnet.net/blog/post/222689676-%E3%80%90%E7%B4%90%E7%B4%84%E8%87%AA%E7%94%B1%E8%A1%8C%E6%87%B6%E4%BA%BA%E5%8C%85%E3%80%91%EF%BC%9A7-%E6%97%A5%E8%A1%8C%E7%A8%8B%E3%80%81%E5%9C%B0%E9%90%B5%E6%94%BB%E7%95%A5" target="_blank">
+                    自由行懶人包
+                  </a>                  
+                </div>
+              </div>  
+            </div>
+          </div>
+      </div>
+    </article>
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "footer.php"; ?>
+
+  </body>
+
+</html>
